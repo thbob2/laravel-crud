b24cdc77c19102ae05e6b892495b299028296ea1
thbob2 <chouaib.dot@hotmail.com>
2019-10-31 22:03:24 +0100

    tags

--- app/Http/Requests/Tags/CreateTagRequest.php
+++ app/Http/Requests/Tags/CreateTagRequest.php
@@ -13,7 +13,7 @@ class CreateTagRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,7 @@ class CreateTagRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        return [ 'name'=> 'required|unique'
             //
         ];
     }
--- app/Http/Requests/Tags/UpdateTagRequest.php
+++ app/Http/Requests/Tags/UpdateTagRequest.php
@@ -13,7 +13,7 @@ class UpdateTagRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,7 @@ class UpdateTagRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        return [ 'name' => 'required|unique'
             //
         ];
     }
