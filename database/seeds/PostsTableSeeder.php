345d6adbed214d5813c9055a1350f1e359c3c805
thbob2 <chouaib.dot@hotmail.com>
2019-11-08 00:12:33 +0100

    installing the theme

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+use App/Post;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $post1 =
+
+    }
+}
