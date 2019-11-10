dbe8bd044f497f1cd253d9044aec15d7562a40e6
thbob2 <chouaib.dot@hotmail.com>
2019-11-10 17:31:58 +0100

    search

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,9 +9,18 @@ use App\Category;
 class WelcomeController extends Controller
 {
     public function index(){
+
+        $search  =  request()->query('search');
+        if($search){
+            $posts = Post::where('title','LIKE',"%{$search}%")->simplepaginate(2);
+        }else{
+            $posts = Post::simplepaginate(4);
+        }
         return view('welcome')
-        ->with('posts', Post::simplepaginate(4))
+        ->with('posts', $posts)
         ->with('tags', Tag::all())
         ->with('categories', Category::all());
     }
+
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@ Dealers Blog
 @endsection
 @section('header')
     <!-- Header -->
-    <header class="header text-center text-white" style="background-image: linear-gradient(-225deg, #000000 0%, #B8DCFF 48%, #1E1A3F 100%);">
+    <header class="header text-center text-white" style="background-image: linear-gradient(-225deg, #1b1b1b 0%, #323232 49%, #b22740 100%);">
             <div class="container">
 
                 <div class="row">
@@ -30,21 +30,25 @@ Dealers Blog
                     <div class="row">
                         <div class="col-md-8 col-xl-9">
                             <div class="row gap-y">
-                                @foreach ($posts as $post)
-
-
-                                    <div class="col-md-6">
-                                        <div class="card border hover-shadow-6 mb-6 d-block">
-                                        <a href="{{ route('blog.show',$post->id) }}"><img class="card-img-top" src="{{url('storage/'.$post->image)}}" alt="Card image cap"></a>
-                                                <div class="p-6 text-center">
-                                                <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="">{{$post->category->name}}</a></p>
-                                                <h5 class="mb-0"><a class="text-dark" href="{{ route('blog.show',$post->id) }}">{{$post->title}}</a></h5>
-                                                </div>
-                                        </div>
+                                @forelse ($posts as $post)
+                                <div class="col-md-6">
+                                    <div class="card border hover-shadow-6 mb-6 d-block">
+                                    <a href="{{ route('blog.show',$post->id) }}"><img class="card-img-top" src="{{url('storage/'.$post->image)}}" alt="Card image cap"></a>
+                                            <div class="p-6 text-center">
+                                            <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="">{{$post->category->name}}</a></p>
+                                            <h5 class="mb-0"><a class="text-dark" href="{{ route('blog.show',$post->id) }}">{{$post->title}}</a></h5>
+                                            </div>
                                     </div>
-                                @endforeach
+                                </div>
+                                @empty
+                                <p class="text-center">
+                                No result found for your request <strong>"{{request()->query('search')}}"</strong>
+                                </p>
+                                @endforelse
+
+
                             </div>
-                            {{$posts->links()}}
+                            {{$posts->appends(['search' => request()->query('search') ])->links()}}
                         </div>
 
 
@@ -53,8 +57,8 @@ Dealers Blog
                     <div class="sidebar px-4 py-md-0">
 
                     <h6 class="sidebar-title">Search</h6>
-                    <form class="input-group" target="#" method="GET">
-                        <input type="text" class="form-control" name="s" placeholder="Search">
+                    <form class="input-group" action="{{route('welcome')}}" method="GET">
+                    <input type="text" class="form-control" name="search" placeholder="Search" value="{{request()->query('search')}}">
                         <div class="input-group-addon">
                             <span class="input-group-text"><i class="ti-search"></i></span>
                         </div>
