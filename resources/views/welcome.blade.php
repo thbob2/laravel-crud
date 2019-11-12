869bac4e695b628916d23a9e625c9a869aa1b18d
thbob2 <chouaib.dot@hotmail.com>
2019-11-12 19:22:36 +0100

    indent fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,38 +25,36 @@ Dealers Blog
 @section('content')
     <!-- Main Content -->
     <main class="main-content">
-            <div class="section bg-gray">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-xl-9">
-                            <div class="row gap-y">
-                                @forelse ($posts as $post)
+        <div class="section bg-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-xl-9">
+                        <div class="row gap-y">
+                            @forelse ($posts as $post)
                                 <div class="col-md-6">
                                     <div class="card border hover-shadow-6 mb-6 d-block">
-                                    <a href="{{ route('blog.show',$post->id) }}"><img class="card-img-top" src="{{url('storage/'.$post->image)}}" alt="Card image cap"></a>
-                                            <div class="p-6 text-center">
+                                        <a href="{{ route('blog.show',$post->id) }}"><img class="card-img-top" src="{{url('storage/'.$post->image)}}" alt="Card image cap"></a>
+                                        <div class="p-6 text-center">
                                             <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="">{{$post->category->name}}</a></p>
                                             <h5 class="mb-0"><a class="text-dark" href="{{ route('blog.show',$post->id) }}">{{$post->title}}</a></h5>
-                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                @empty
-                                <p class="text-center">
-                                No result found for your request <strong>"{{request()->query('search')}}"</strong>
-                                </p>
-                                @endforelse
+                            @empty
+                                <p class="text-center">No result found for your request <strong>"{{request()->query('search')}}"</strong></p>
+                            @endforelse
 
 
-                            </div>
-                            {{$posts->appends(['search' => request()->query('search') ])->links()}}
                         </div>
+                        {{$posts->appends(['search' => request()->query('search') ])->links()}}
+                    </div>
 
 
 
-                @include('partials.sidebar')
+                    @include('partials.sidebar')
 
-            </div>
+                </div>
             </div>
         </div>
-        </main>
+    </main>
 @endsection
